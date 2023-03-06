0a7eafd2515fcaf76a3deab123ed7a09d6451bef
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-06 23:32:56 +0300

    linter configured

--- Person.rb
+++ Person.rb
@@ -2,16 +2,16 @@ class Person
   attr_accessor :id, name, age
 
 
-  private
-
-  def of_age?
-    return false if age <= 18
+  def can_use_services?
+    return unless age == 18 && parent_permission == true
 
     true
   end
 
-  def can_use_services?
-    return unless age == 18 && parent_permission == true
+  private
+
+  def of_age?
+    return false if age <= 18
 
     true
   end
