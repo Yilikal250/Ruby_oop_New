9322a12eb21a08b9e1d1375b2736351f95a03464
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2023-03-07 11:20:53 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,10 +1,7 @@
 class Person
   attr_accessor :id, name, age
-
-
   def can_use_services?
     return unless age == 18 && parent_permission == true
-
     true
   end
 
@@ -12,7 +9,6 @@ class Person
 
   def of_age?
     return false if age <= 18
-
     true
   end
 end
