3b06c0b091091f911b33119d67035012cc7bef9f
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-14 23:08:40 +0300

    Added classes

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
-class Person
+require_relative './nameable'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -13,9 +16,20 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalizedPerson = CapitalizeDecorator.new(person)
+# capitalizedPerson.correct_name
+# capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+# capitalizedTrimmedPerson.correct_name
