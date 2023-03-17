66a55873e7f13593f67438b1e9bb5ca9be6d73cf
Yilikal250 <teshomeyilikal250@gmail.com>
2023-03-17 20:50:08 +0300

    added rental ,classroom,book class

--- student.rb
+++ student.rb
@@ -1,9 +1,15 @@
+require './classroom'
 class Student < Person
   attattr_accessor :@classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  def initialize(*person, classroom)
+    super(*person)
     @classroom = classroom
-    super(age, name, parent_permission: parent_permission)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
